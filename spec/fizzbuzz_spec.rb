cfba89e82d14736a09e53fb165c317cd0c3d489c
Evan Thomas <evanthomas0292@gmail.com>
2023-04-04 11:30:54 +0100

    Refactor to use %

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,8 +5,16 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
 
+  it 'returns "fizz" when passed 9' do
+    expect(fizzbuzz(9)).to eq 'fizz'
+  end
+
   it 'returns "buzz" when passed 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
   end
 
   it 'returns "fizzbuzz" when passed 15' do
@@ -15,5 +23,9 @@ describe 'fizzbuzz' do
 
   it 'returns 1 when passed 1' do
     expect(fizzbuzz(1)).to eq 1
+  end
+  
+  it 'returns 2 when passed 2' do
+    expect(fizzbuzz(2)).to eq 2
   end
 end
